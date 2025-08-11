b42622c20733079e2d7be3a51cae63758bf0ab4d
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-08-11 09:31:16 +0300

    Updated by removing update existing documents in server.js

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log('MongoDB connected');
   
   // --- ADDED: Call the one-time function here ---
-  updateExistingDocuments();
 
 }).catch(err => console.error('MongoDB connection error:', err));
 
